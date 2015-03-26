6b45cfe0886ac6f1acf7892223f603f566162b8d
facciolo <luca@siryus.com.br>
2015-03-25 22:48:59 -0300

    parte 3

--- Forca.py
+++ Forca.py
@@ -5,47 +5,48 @@ erros = 0
 lista1 = []
 lista = open("entrada.txt","r+",encoding = "utf-8")
 leitura = lista.readlines()
-palavra = random.choice(leitura)
-palavra.index
+palavra = random.choice(leitura).lower()#comando para tirar acento
 a = ''
-
-def letras(a):
-    global erros
-    a = input("digite uma letra: ").lower()
-    while (erros != 6):
-        if a in palavra and len(a)==1:
-            if a in digitadas:
-                print("voce ja digitou essa letra")
-                break
-            print("Acertou , digite outra letra")
-            digitadas.append(a)
-            acertos.append(a)
-            print("letras digitadas",digitadas)
-            break
-        elif a not in palavra and len(a)==1:
-            if a in digitadas:
-                print("voce ja digitou essa letra")
-                break
-            print("Voce errou , tente novamente")
-            erros = erros + 1
-            digitadas.append(a)
-            print("letras digitadas",digitadas)
-            print("total de 6 tentativas.",erros,"erros atuais")
-            break
-        else:
-            print("Bugou , tente novamente")
-            break
-        
-
-
 print(palavra)#teste
-while(erros != 6):
-    letras(a)
+while (erros < 6 or acertos != palavra):
+    a = input("digite uma letra: ").lower()
+    if a in palavra and len(a)==1:
+            if a in digitadas:
+                print("voce ja digitou essa letra")
+            else:
+                print("Acertou , digite outra letra")
+                digitadas.append(a)
+                acertos.append(a)
+                print("letras digitadas",digitadas)
+    elif a not in palavra and len(a)==1:
+                if a in digitadas:
+                    print("voce ja digitou essa letra")
+                else:
+                    print("Voce errou , tente novamente")
+                    erros = erros + 1
+                    digitadas.append(a)
+                    print("letras digitadas",digitadas)
+                    print("total de 6 tentativas.",erros,"erros atuais")
+                    #1-
+    else:
+            print("Bugou , tente novamente")
+            
+                """1-if erros == 1:
+                                #desenhar cabeça
+                    if erros == 2:
+                                #desenhar tronco
+                    erros == 3:
+                                #desenhar braço esquerdo
+                    erros == 4:
+                                #desenhar braço direito
+                    erros == 5:
+                                #desenhar perna esquerda
+                    erros == 6:
+                                #desenhar perna direita
+                                print("voce perdeu")"""
 
-if erros != 6:
-    print("voce ganhou")
-    print("a palavra era", palavra)
-else:
-    print("voce perdeu")
-    print("a palavra era", palavra)
+
+
+
+
 
