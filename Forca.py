fdb2c880553ac0c7acc217eec418200224667864
facciolo <luca@siryus.com.br>
2015-03-30 20:36:55 -0300

    9 quase pronto

--- Forca.py
+++ Forca.py
@@ -1,36 +1,41 @@
-import random
+from random import choice
 import turtle    
-import time
-
-
-
+from time import sleep
+import unicodedata
+#Listas
 digitadas = []
 acertos = []
 erros = 0
 lista1 = []
-lista = open("entrada.txt","r",encoding = "utf-8")
+#Escolhendo a palavra 
+lista = open("entrada.txt","r+",encoding = "utf-8")
 leitura = lista.readlines()
-palavra = random.choice(leitura).lower().strip()# + comando para tirar acento
+if leitura != "":
+    palavra = choice(leitura).lower().strip()# + comando para tirar acento
 a = ''
-
+for i in palavra:
+    if i not in lista1 and i != " ":
+        lista1.append(i)
+        lista1.sort()
+#Cursores da interface
 window = turtle.Screen()
 window.bgcolor("black")
 window.title("Forca")
-window.bgpic()
+window.bgpic("planofundo.gif")
 
 tartaruga = turtle.Turtle()  # Cria um objeto "desenhador"
 tartaruga.speed(1000)  # define a velocidade
 tartaruga.penup()       # Remova e veja o que acontece
-tartaruga.setpos(-100,0)
+tartaruga.setpos(200,0)
 tartaruga.pendown()
-tartaruga.color("Red")
+tartaruga.color("white")
 
 tartaruga2 = turtle.Turtle()
 tartaruga2.speed(1000)
 tartaruga2.penup()
 tartaruga2.setpos(-100,0)
 tartaruga2.pendown()
-tartaruga2.color("red")
+tartaruga2.color("white")
 tartaruga2.hideturtle()
 
 tartaruga3 = turtle.Turtle()
@@ -38,105 +43,89 @@ tartaruga3.speed(1000)
 tartaruga3.penup()
 tartaruga3.setpos(-100,0)
 tartaruga3.pendown()
-tartaruga3.color("red")
+tartaruga3.color("white")
 tartaruga3.hideturtle()
 
 tartaruga4 = turtle.Turtle()
 tartaruga4.speed(1000)
 tartaruga4.penup()
-tartaruga4.setpos(-250,-150)
+tartaruga4.setpos(-250,-250)
 tartaruga4.pendown()
-tartaruga4.color("red")
+tartaruga4.color("white")
 tartaruga4.hideturtle()
 
 tartaruga5 = turtle.Turtle()
 tartaruga5.speed(1000)
 tartaruga5.penup()
-tartaruga5.setpos(-247,-148)
+tartaruga5.setpos(-247,-248)
 tartaruga5.pendown()
-tartaruga5.color("red")
+tartaruga5.color("white")
 tartaruga5.hideturtle()
-
-def cabeça():
-    tartaruga.penup()
-    tartaruga.setpos(-50,200)
-    tartaruga.pendown()
-    tartaruga.rt(90)
-    tartaruga.circle(20)
-    
-def tronco():
-    tartaruga.penup()
-    tartaruga.setpos(-50,160)
-    tartaruga.pendown()
-    tartaruga.rt(270)
-    tartaruga.forward(65)
-    
-def braço_esquerdo():
-    tartaruga.penup()
-    tartaruga.setpos(-50,140)
-    tartaruga.pendown()
-    tartaruga.rt(260)
-    tartaruga.forward(30)
-    
-def braço_direito():
-    tartaruga.penup()
-    tartaruga.setpos(-50,140)
-    tartaruga.pendown()
-    tartaruga.rt(230)
-    tartaruga.forward(30)
-    
-def perna_esquerda():
-    tartaruga.penup()
-    tartaruga.setpos(-50,95)
-    tartaruga.pendown()
-    tartaruga.rt(170)
-    tartaruga.forward(50)
-    
-def perna_direita():
-    tartaruga.penup()
-    tartaruga.setpos(-50,95)
-    tartaruga.pendown()
-    tartaruga.rt(110)
-    tartaruga.forward(50)
-    
+#Desenhos na interface
+def tronco(erros):
+    if erros ==1:#cabeça
+        tartaruga.penup()
+        tartaruga.setpos(250,200)
+        tartaruga.pendown()
+        tartaruga.rt(90)
+        tartaruga.circle(20)    
+    if erros == 2:#tronco
+        tartaruga.penup()
+        tartaruga.setpos(250,160)
+        tartaruga.pendown()
+        tartaruga.rt(270)
+        tartaruga.forward(65)    
+    if erros == 3:#braço esquerdo
+        tartaruga.penup()
+        tartaruga.setpos(250,140)
+        tartaruga.pendown()
+        tartaruga.rt(260)
+        tartaruga.forward(30)    
+    if erros == 4:#braço direito
+        tartaruga.penup()
+        tartaruga.setpos(250,140)
+        tartaruga.pendown()
+        tartaruga.rt(230)
+        tartaruga.forward(30)    
+    if erros == 5:#perna esquerda
+        tartaruga.penup()
+        tartaruga.setpos(250,95)
+        tartaruga.pendown()
+        tartaruga.rt(170)
+        tartaruga.forward(50)
+    if erros == 6:#perna direita
+        tartaruga.penup()
+        tartaruga.setpos(250,95)
+        tartaruga.pendown()
+        tartaruga.rt(110)
+        tartaruga.forward(50)
+#Textos na interface    
 def escrever_textostemporal(c):
     tartaruga2.penup()
-    tartaruga2.setpos(-40,-100)
+    tartaruga2.setpos(-250,-130)
     tartaruga2.write(c,font = ("arial",15,"normal"))
-    time.sleep(1)
+    sleep(0.8)
     tartaruga2.clear()
-    
 def escrever_textos(b):
     tartaruga.penup()
     tartaruga.setpos(0,250)
-    tartaruga.write(b,font = ("arial",15,"normal"))
-    
+    tartaruga.write(b,font = ("arial",15,"normal"))    
 def escrever_digitadas():
     digitadas.append(a)
     tartaruga3.penup()
-    tartaruga3.setpos(50,-200)
+    tartaruga3.setpos(-60,-200)
     tartaruga3.pendown()
     tartaruga3.write(digitadas,font = ("arial",15,"normal"))
-    time.sleep(1)
-    tartaruga3.clear()
-    
-def escrever_textocomtitulo(titulo,conteudo):
-    tartaruga.penup()
-    tartaruga.setpos(0,-70)
-    tartaruga.pendown()
-    tartaruga.write(titulo,font = ("arial",20,"normal"))
-    tartaruga.penup()
-    tartaruga.setpos(0,-100)
-    tartaruga.write(conteudo,font = ("arial",15,"normal"))
-    
+    sleep(1.5)
+    tartaruga3.clear()    
 def escrever_textocomtitulo2(titulo,conteudo):
     tartaruga.penup()
-    tartaruga.setpos(100,200)
+    tartaruga.setpos(-300,200)
     tartaruga.pendown()
     tartaruga.write(titulo,font = ("arial",20,"normal"))
     tartaruga.penup()
-    tartaruga.setpos(100,170)
-    tartaruga.write(conteudo,font = ("arial",15,"normal"))
+    tartaruga.setpos(-300,170)
+    tartaruga.write(conteudo,font = ("arial",20,"normal"))
     
 #DESENHO FORCA:    
 for i in range(1):
@@ -159,25 +148,17 @@ for i in palavra:
     tartaruga4.forward(5)
     tartaruga4.pendown()
     
-for i in palavra:
-    
-    if i not in lista1 and i != " ":
-        lista1.append(i)
-        lista1.sort()
-
 escrever_textocomtitulo2(palavra,lista1)
 while (erros != 6 and acertos != lista1):
-    a = window.textinput("","digite uma letra").lower()
-    
-   
+    a = window.textinput("","digite uma letra ou a palavra misteriosa").lower()
     if a == palavra:
         break
-    if a in palavra and len(a)==1:
+    if a != " " and a in lista1 and len(a)==1 :
                 if a in digitadas:
                     escrever_textostemporal("Voce ja digitou essa letra!")
                 else:
                     escrever_textostemporal("acertou")
-                    z= 0
+                    z = 0
                     while z < len(palavra):
                         if a == palavra[z]:
                             tartaruga5.penup()
@@ -191,7 +172,7 @@ while (erros != 6 and acertos != lista1):
                     digitadas.sort()                    
                     acertos.append(a)
                     acertos.sort()
-    elif a not in palavra and len(a)==1:
+    elif a not in lista1 and len(a)==1:
                 if a in digitadas:
                     escrever_textostemporal("voce ja digitou essa letra")
                 else:
@@ -199,28 +180,16 @@ while (erros != 6 and acertos != lista1):
                     erros = erros + 1
                     escrever_digitadas()
                     digitadas.sort()
-                    escrever_textostemporal(('total de 6 tentativas.',erros,"erro(s) atuai(s)"))
-                    if erros == 1:
-                                cabeça()
-                    if erros == 2:
-                                tronco()
-                    if erros == 3:
-                                braço_esquerdo()
-                    if erros == 4:
-                                braço_direito()
-                    if erros == 5:
-                                perna_esquerda()
-                    if erros == 6:
-                                perna_direita()
+                    tronco(erros)
     else:
         escrever_textostemporal("Bugou , tente novamente")
 if erros == 6:
      escrever_textostemporal("Voce perdeu!!")
-     escrever_textocomtitulo2("A palavra era",palavra)
-     time.sleep(2)
+     escrever_textocomtitulo2("A palavra era:",palavra)
+     sleep(2)
      window.bye()
 else:
      escrever_textostemporal("Voce ganhou!")
-     time.sleep(2)
+     sleep(2)
      window.bye()
 window.exitonclick()
